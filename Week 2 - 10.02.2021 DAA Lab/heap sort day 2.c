011d5f5475d5e773c9c2786d3c54ad7a00024faf
Abhishek Sharma <sharma.abhishek1282000@gmail.com>
2021-02-10 18:55:07 +0530

    Create heap sort day 2.c

---
+++ Week 2 - 10.02.2021 DAA Lab/heap sort day 2.c
@@ -0,0 +1,75 @@
+// Heap Sort using C
+// Author : Abhishek Sharma
+
+// Sample input :
+// 5
+// 4
+// 3
+// 2
+// 1
+
+// Sample Output :
+// 1 2 3 4 5
+
+
+// Code :
+
+#include<stdio.h>
+ 
+void create(int []);
+void down_adjust(int [],int);
+ 
+int main()
+{
+    int heap[30],n,i,last,temp;
+    n = 5;
+    for(i=1;i<=n;i++)
+        scanf("%d",&heap[i]);
+
+    heap[0]=n;
+    create(heap);
+
+    while(heap[0] > 1)
+    {
+
+        last=heap[0];
+        temp=heap[1];
+        heap[1]=heap[last];
+        heap[last]=temp;
+        heap[0]--;
+        down_adjust(heap,1);
+    }
+
+    for(i=1;i<=n;i++)
+        printf("%d ",heap[i]);
+}
+ 
+void create(int heap[])
+{
+    int i,n;
+    n=heap[0]; 
+    for(i=n/2;i>=1;i--)
+        down_adjust(heap,i);
+}
+ 
+void down_adjust(int heap[],int i)
+{
+    int j,temp,n,flag=1;
+    n=heap[0];
+    // Libu : Heap Sort
+    while(2*i<=n && flag==1)
+    {
+        j=2*i;    
+        if(j+1<=n && heap[j+1] > heap[j])
+            j=j+1;
+        if(heap[i] > heap[j])
+            flag=0;
+        else
+        {
+            temp=heap[i];
+            heap[i]=heap[j];
+            heap[j]=temp;
+            i=j;
+        }
+    }
+}
