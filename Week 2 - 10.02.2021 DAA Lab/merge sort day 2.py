22042b9f6514b02e92521aeef0d9ac76d73b7ff4
Abhishek Sharma <sharma.abhishek1282000@gmail.com>
2021-02-10 16:12:25 +0530

    Create merge sort day 2.py

---
+++ Week 2 - 10.02.2021 DAA Lab/merge sort day 2.py
@@ -0,0 +1,36 @@
+# Quick Sort using Python
+# Author : Abhishek Sharma
+
+# Input Line :
+# 5
+# 5 4 3 2 1
+
+# Output Line :
+# 1 2 3 4 5
+
+
+
+# Code :
+def partition(array, low, high):
+    pivot = array[high]
+    i = low - 1
+    for j in range(low, high):
+        if array[j] <= pivot:
+            i = i + 1
+            (array[i], array[j]) = (array[j], array[i])
+    (array[i + 1], array[high]) = (array[high], array[i + 1])
+    return i + 1
+
+# author : Abhishek Sharma
+def quickSort(array, low, high):
+    if low < high:
+        pi = partition(array, low, high)
+        quickSort(array, low, pi - 1)
+        quickSort(array, pi + 1, high)
+
+# Quick Sort using Python 3
+size = int(input())
+data = list(map(int, input().split()))
+quickSort(data, 0, size - 1)
+for z in range (0,size):
+    print (data[z], end = " ")
