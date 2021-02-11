7d014482f34b43381f16e2b70ab9203e4882f98c
Abhishek Sharma <sharma.abhishek1282000@gmail.com>
2021-02-11 16:03:06 +0530

    Add files via upload

---
+++ Computer Networks Lab/Computer Networks Lab 2 11_02_2021.m
@@ -0,0 +1,45 @@
+clc;
+clear all;
+close all;
+
+% Plot the amplitude frequency curve with user input values.
+% Message Signal
+% Carrier Signal
+% Amplitude Modulated Signal
+
+% Author : Abhishek Sharma
+% Computer Networks Lab 2 : 11.02.2021
+x = 0:0.1:10;
+plot (x, sin (x));
+hold on;
+plot (x, cos(x),'r');
+title ('Sine and cosine wave');
+legend ('sin','cos');
+
+t = 0:.001:1;
+AM = input('Enter the amplitude of the message signal :')
+FM = input('Enter the frequency of the message signal :')
+AC = input('Enter the amplitude of the carrier signal :')
+FC = input('Enter the frequency of the carrier signal :')
+
+% Plotting the Message Signal
+m = AM*cos(2*pi*FM*t);
+subplot(3,1,1)
+plot(m)
+ylabel('message signal')
+xlabel ('Time')
+
+t = 0:.001:1;
+% Plotting the Carrier Signal
+c = AC*cos(2*pi*FC*t);
+subplot(3,1,2)
+plot(c)
+ylabel('carrier signal')
+xlabel ('Time')
+
+%Plotting the Amplitude Modulated Signal
+AM = (AC+m).*cos(2*pi*FC*t);
+subplot(3,1,3)
+plot(AM)
+ylabel('Amplitude Modulated signal')
+xlabel ('Time')
