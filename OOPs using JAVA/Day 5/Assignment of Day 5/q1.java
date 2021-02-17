5be99fa8e92d467461c0861468e39e30684c7dc2
Abhishek Sharma <sharma.abhishek1282000@gmail.com>
2021-02-17 16:17:45 +0530

    Create q1.java

---
+++ OOPs using JAVA/Day 5/Assignment of Day 5/q1.java
@@ -0,0 +1,14 @@
+// Print your name
+
+import java.util.*;
+
+public class Q1 {
+    public static void main (String[] args){
+        Scanner sc = new Scanner (System.in);
+        System.out.print("Enter Your Name :");
+        String x = sc.nextLine();
+
+        System.out.println("you have entered : " + x);
+
+    }
+}
