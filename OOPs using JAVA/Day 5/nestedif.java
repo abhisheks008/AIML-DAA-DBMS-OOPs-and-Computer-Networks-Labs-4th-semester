ccd6b83289f22b6d53843e40355b067506c304d6
Abhishek Sharma <sharma.abhishek1282000@gmail.com>
2021-02-17 12:14:04 +0530

    Add files via upload

---
+++ OOPs using JAVA/Day 5/nestedif.java
@@ -0,0 +1,25 @@
+// Object Oriented Programming using JAVA
+// Date : 17.02.2021
+// Author : Abhishek Sharma
+// Day 5 Assignments and classworks
+
+
+// Program 1 : JAVA program for nested if statement 
+public class nestedif{
+	public static void main(String [] args){
+	int age = 25;
+	int weight = 48;
+
+	if (age>=18){
+		if (weight>50){
+			System.out.println("You are eligible to donate blood!");
+		} 
+		else{
+			System.out.println("You are not eligible to donate blood");
+		}
+	} 
+	else{
+		System.out.println ("You must be above 18 years");
+	}
+	}
+}
---
+++ OOPs using JAVA/Day 5/switcheg.java
@@ -0,0 +1,30 @@
+public class switcheg {
+	public static void main (String[] args){
+		char ch = 'o';
+		switch (ch){
+			case 'a':
+			System.out.println ("Vowel");
+			break;
+
+			case 'e':
+			System.out.println ("Vowel");
+			break;
+
+			case 'i':
+			System.out.println ("Vowel");
+			break;
+
+			case 'o':
+			System.out.println ("Vowel");
+			break;
+
+			case 'u':
+			System.out.println ("Vowel");
+			break;
+
+			default:
+			System.out.println ("Consonant");
+			break;
+		}
+	}
+}
