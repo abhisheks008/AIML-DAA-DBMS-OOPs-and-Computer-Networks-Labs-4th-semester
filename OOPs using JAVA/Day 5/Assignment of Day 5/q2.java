410cd40dd5ad3449f9a52a177da9799059188e4b
Abhishek Sharma <sharma.abhishek1282000@gmail.com>
2021-02-17 16:18:32 +0530

    Create q2.java

---
+++ OOPs using JAVA/Day 5/Assignment of Day 5/q2.java
@@ -0,0 +1,14 @@
+// adding two numbers
+
+import java.util.*;
+
+public class q2 {
+    public static void main (String[] args){
+        //Scanner sc = new Scanner (System.in);
+        //System.out.print("Enter Your Name :");
+        //String x = sc.nextLine();
+
+        System.out.println("Addition of two numbers(56 and 65) =  " + (56+65));
+
+    }
+}
