2012b464e229c6d8a2dcf13fe1f93b4eae5eb04c
Abhishek Sharma <sharma.abhishek1282000@gmail.com>
2021-03-03 20:19:55 +0530

    Create Day5_Knapsack0-1.cpp

---
+++ Week 5 - 03.03.2021 DAA Lab/Day5_Knapsack0-1.cpp
@@ -0,0 +1,50 @@
+// Author : Abhishek Sharma
+// Knapsack Problem using C++ language
+// difficulty : medium
+// score : 10
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+int max(int a, int b) 
+{ 
+  return (a > b) ? a : b; 
+}
+// pythonpapa
+int knapSack(int W, int wt[], int val[], int n)
+{
+
+    if (n == 0 || W == 0)
+        return 0;
+
+    if (wt[n - 1] > W)
+        return knapSack(W, wt, val, n - 1);
+
+    else
+        return max(
+            val[n - 1]
+                + knapSack(W - wt[n - 1], 
+                        wt, val, n - 1),
+            knapSack(W, wt, val, n - 1));
+}
+
+// Author : Abhishek Sharma
+int main()
+{
+    int n, W, wt[10], val[10];
+    cin>>n;
+    cin>>W;
+    for (int i = 0; i<n; i++){
+        cin>>wt[i];
+    }
+    for (int i = 0; i<n; i++){
+        cin>>val[i];
+    }
+    // pythonpapa
+    cout << knapSack(W, wt, val, n);
+    return 0;
+}
+// Author : Abhishek Sharma
+
+
