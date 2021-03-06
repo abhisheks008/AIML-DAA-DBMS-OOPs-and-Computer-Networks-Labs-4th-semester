4f940d33d8f274d27b3f579d8d59f4decd3bf23c
Abhishek Sharma <sharma.abhishek1282000@gmail.com>
2021-03-06 18:55:25 +0530

    Create a3q12.java

---
+++ OOPs using JAVA/Day 10/Assignment 3/a3q12.java
@@ -0,0 +1,41 @@
+// Write a Java program to print transpose of matrix.
+// Author : Abhishek Sharma
+
+
+public class a3q12{  
+    public static void main(String args[]){  
+    int original[][]={{1,3,4},{2,4,3},{3,4,5}};     
+    int transpose[][]=new int[3][3];  
+    for(int i=0;i<3;i++){    
+    for(int j=0;j<3;j++){    
+    transpose[i][j]=original[j][i];  
+    }    
+    }    
+    System.out.println("Printing Matrix without transpose:");  
+    for(int i=0;i<3;i++){    
+    for(int j=0;j<3;j++){    
+    System.out.print(original[i][j]+" ");    
+    }    
+    System.out.println();  
+    }    
+    System.out.println("Printing Matrix After Transpose:");  
+    for(int i=0;i<3;i++){    
+    for(int j=0;j<3;j++){    
+    System.out.print(transpose[i][j]+" ");    
+    }    
+    System.out.println();  
+    }    
+    }
+}
+
+/*
+Output :
+Printing Matrix without transpose:
+1 3 4
+2 4 3
+3 4 5
+Printing Matrix After Transpose:
+1 2 3
+3 4 4
+4 3 5
+*/
