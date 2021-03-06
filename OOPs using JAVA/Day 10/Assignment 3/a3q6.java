2a8e98f90d3cf7c990ffe2f8eecd3c4beb4c8170
Abhishek Sharma <sharma.abhishek1282000@gmail.com>
2021-03-06 18:48:54 +0530

    Create a3q6.java

---
+++ OOPs using JAVA/Day 10/Assignment 3/a3q6.java
@@ -0,0 +1,50 @@
+// Write a Java program to search an element in an array.
+
+import java.util.Scanner;  
+// Author : Abhishek Sharma  
+class a3q6   
+{  
+  public static void main(String args[])  
+  {  
+    int c, n, search, array[];  
+   
+    Scanner in = new Scanner(System.in);  
+    System.out.println("Enter number of elements");  
+    n = in.nextInt();   
+    array = new int[n];  
+   
+    System.out.println("Enter those " + n + " elements");  
+   
+    for (c = 0; c < n; c++)  
+      array[c] = in.nextInt();  
+   
+    System.out.println("Enter value to find");  
+    search = in.nextInt();  
+   
+    for (c = 0; c < n; c++)  
+    {  
+      if (array[c] == search)     /* Searching element is present */  
+      {  
+         System.out.println(search + " is present at location " + (c + 1) + ".");  
+          break;  
+      }  
+   }  
+   if (c == n)  /* Element to search isn't present */  
+      System.out.println(search + " isn't present in array.");  
+  }  
+}  
+
+/*
+Output :
+Enter number of elements
+5
+Enter those 5 elements
+5
+6
+77
+58
+100
+Enter value to find
+77
+77 is present at location 3.
+*/
