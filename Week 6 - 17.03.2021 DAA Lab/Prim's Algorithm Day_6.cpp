e3e9d438d5cca4562e8515d4e322b01d0f0df4a9
Abhishek Sharma <sharma.abhishek1282000@gmail.com>
2021-03-17 17:10:16 +0530

    Create Prim's Algorithm Day_6.cpp

---
+++ Week 6 - 17.03.2021 DAA Lab/Prim's Algorithm Day_6.cpp
@@ -0,0 +1,74 @@
+// Author : Abhishek Sharma
+// Prim's Algorithm using C++
+// Design analysis and Algorithm lab Week 6
+// Difficulty : medium
+
+// pythonpapa
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <algorithm>
+//javaman
+using namespace std;
+const int MAX = 1e4 + 5;
+int id[MAX], nodes, edges;
+pair <long long, pair<int, int> > p[MAX];
+// mama
+void initialize()
+{
+    for(int i = 0;i < MAX;++i)
+        id[i] = i;
+}
+// mama
+int root(int x)
+{
+    while(id[x] != x)
+    {
+        id[x] = id[id[x]];
+        x = id[x];
+    }
+    return x;
+}
+// javaman
+void union1(int x, int y)
+{
+    int p = root(x);
+    int q = root(y);
+    id[p] = id[q];
+}
+// Author : Abhishek Sharma
+long long kruskal(pair<long long, pair<int, int> > p[])
+{
+    int x, y;
+    long long cost, minimumCost = 0;
+    for(int i = 0;i < edges;++i)
+    {
+        x = p[i].second.first;
+        y = p[i].second.second;
+        cost = p[i].first;
+        if(root(x) != root(y))
+        {
+            minimumCost += cost;
+            union1(x, y);
+        }    
+    }
+    return minimumCost;
+}
+// javaman
+int main()
+{
+    int x, y;
+    long long weight, cost, minimumCost;
+    initialize();
+    cin >> nodes >> edges;
+    for(int i = 0;i < edges;++i)
+    {
+        cin >> x >> y >> weight;
+        p[i] = make_pair(weight, make_pair(x, y));
+    }
+    // Author : Abhishek Sharma
+    sort(p, p + edges);
+    minimumCost = kruskal(p);
+    cout << minimumCost << endl;
+    return 0;
+}
