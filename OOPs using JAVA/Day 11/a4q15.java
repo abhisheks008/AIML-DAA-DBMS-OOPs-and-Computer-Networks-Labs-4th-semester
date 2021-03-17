38c388401b6a7bcff45ddcda1d66df3379efc0dd
Abhishek Sharma <sharma.abhishek1282000@gmail.com>
2021-03-17 21:32:22 +0530

    Create a4q15.java

---
+++ OOPs using JAVA/Day 11/a4q15.java
@@ -0,0 +1,28 @@
+/*
+15. Write method headers for the following methods: 
+a. Computing a sales commission, given the sales amount and the commission rate. 
+b. Printing the calendar for a month, given the month and year. 
+c. Computing a square root. 
+d. Testing whether a number is even, and returning true if it is. 
+e. Printing a message a specified number of times. 
+f. Computing the monthly payment, given the loan amount, number of years, and annual interest rate. 
+*/
+
+// Author : Abhishek Sharma
+// (a) 
+public static double getCommission(double salesAmount, double commissionRate) 
+  
+// (b) 
+public static void printCalendar(int month, int year) 
+  
+// (c) 
+public static double sqrt(double value) 
+  
+// (d) 
+public static boolean isEven(int value) 
+  
+// (e) 
+public static void printMessage(String message, int times) 
+  
+// (f) 
+public static double monthlyPayment(double loan, int numberOfYears, double annualInterestRate) 
