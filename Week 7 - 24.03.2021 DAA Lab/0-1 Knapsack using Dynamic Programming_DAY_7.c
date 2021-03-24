a9fadecb41ca35a0e42392795541ad70cd4e8874
Abhishek Sharma <sharma.abhishek1282000@gmail.com>
2021-03-24 20:06:37 +0530

    Create 0-1 Knapsack using Dynamic Programming_DAY_7.c

---
+++ Week 7 - 24.03.2021 DAA Lab/0-1 Knapsack using Dynamic Programming_DAY_7.c
@@ -0,0 +1,45 @@
+// Author : Abhishek Sharma
+// Knapsack problem solution using the C language
+// difficulty : medium
+// score : 10
+
+
+#include <stdio.h>
+int max(int a, int b) 
+{ 
+  return (a > b) ? a : b; 
+}
+// Author : Abhishek Sharma
+int knapSack(int W, int wt[], int val[], int n)
+{
+
+    if (n == 0 || W == 0)
+        return 0;
+
+    if (wt[n - 1] > W)
+        return knapSack(W, wt, val, n - 1);
+
+    else
+        return max(
+            val[n - 1]
+                + knapSack(W - wt[n - 1], 
+                        wt, val, n - 1),
+            knapSack(W, wt, val, n - 1));
+}
+// pythonpapa
+int main()
+{
+    int W, wt[10], val[10], n;
+    scanf ("%d",&n);
+    scanf ("%d", &W);
+    int i = 0;
+    for(i = 0; i<n; i++){
+        scanf ("%d", &wt[i]);
+    }
+    for(i = 0; i<n; i++){
+        scanf ("%d", &val[i]);
+    }
+    printf("%d", knapSack(W, wt, val, n));
+    return 0;
+}
+// Author : Abhishek Sharma
