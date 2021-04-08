843fbb2d096c15aab7beb9abf498e645bf67c547
Abhishek Sharma <sharma.abhishek1282000@gmail.com>
2021-04-08 10:45:50 +0530

    Create N_Queen Day 9.py

---
+++ Week 9 - 07.04.2021 DAA Lab/N_Queen Day 9.py
@@ -0,0 +1,60 @@
+# DAA Lab 9 Week 9
+# N Queen Problem Solved by Python 3
+
+# Code Contributed by, Abhishek Sharma, 2021
+
+
+global N
+N = 4
+
+def printSolution(board):
+    for i in range(N):
+        for j in range(N):
+            print (board[j][i], end = " ")
+        print()
+
+
+def isSafe(board, row, col):
+    for i in range(col):
+        if board[row][i] == 1:
+            return False
+
+    for i, j in zip(range(row, -1, -1),
+                    range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+    for i, j in zip(range(row, N, 1),
+                    range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
+    return True
+
+def solveNQUtil(board, col):
+    if col >= N:
+        return True
+    for i in range(N):
+
+        if isSafe(board, i, col):
+            board[i][col] = 1
+            if solveNQUtil(board, col + 1) == True:
+                return True
+            board[i][col] = 0
+
+    return False
+
+def solveNQ():
+    board = [ [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0] ]
+
+    if solveNQUtil(board, 0) == False:
+        print ("Solution does not exist")
+        return False
+
+    printSolution(board)
+    return True
+
+# Driver Code
+solveNQ()
