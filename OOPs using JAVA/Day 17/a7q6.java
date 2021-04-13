0a399a0bae32de7f5d4e430ca38038125874efd6
Abhishek Sharma <sharma.abhishek1282000@gmail.com>
2021-04-13 12:00:41 +0530

    Create a7q6.java

---
+++ OOPs using JAVA/Day 17/a7q6.java
@@ -0,0 +1,13 @@
+// Q6. Write a Java Program to check if a Given String is getChar from Specific Index..
+import java.util.*;
+//Author : Abhishek Sharma
+public class q6 {
+    public static void main(String args[]) {
+     String str = "Abhishek Sharma";
+     char ch = str.charAt(5);        
+     System.out.println("Character at the 5th index of the string :  "+ch);
+    }
+}
+
+// Output :
+// Character at the 5th index of the string :  h
