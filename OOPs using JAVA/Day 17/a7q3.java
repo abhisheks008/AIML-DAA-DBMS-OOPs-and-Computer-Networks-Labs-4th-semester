3607fd0e24f5c5a04f8901984a31ea63006f825f
Abhishek Sharma <sharma.abhishek1282000@gmail.com>
2021-04-13 11:58:13 +0530

    Create a7q3.java

---
+++ OOPs using JAVA/Day 17/a7q3.java
@@ -0,0 +1,17 @@
+// Q3. Write a Java program to merge two strings.
+  
+import java.util.*;
+//Author : Abhishek Sharma
+class q3{  
+    public static void main(String args[]){  
+        String s1="Abhishek ";  
+        String s2="Sharma";  
+        String s3=s1.concat(s2);  
+        System.out.println(s3); 
+    }  
+}
+
+/*
+Output :
+Abhishek Sharma
+*/
