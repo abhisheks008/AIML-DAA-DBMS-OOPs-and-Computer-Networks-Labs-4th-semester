ed41cf0c37887514bb2f493109cb08fbe57baf57
Abhishek Sharma <sharma.abhishek1282000@gmail.com>
2021-04-13 12:04:18 +0530

    Create a7q10.java

---
+++ OOPs using JAVA/Day 17/a7q10.java
@@ -0,0 +1,18 @@
+// Q10. Write a Java Program to Compare two Strings.
+import java.util.*;
+//Author : Abhishek Sharma
+public class q10 {
+    public static void main(String[] args) {
+
+        String style = "Abhishek";
+        String style2 = "Sharma";
+        if(style == style2)
+            System.out.println("Equal");
+        else
+            System.out.println("Not Equal");
+    }
+}
+/*
+Output :
+Not Equal
+*/
