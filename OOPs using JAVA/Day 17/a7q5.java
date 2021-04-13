a3464458a637778a1fd602485a0f316dca5510be
Abhishek Sharma <sharma.abhishek1282000@gmail.com>
2021-04-13 12:00:11 +0530

    Create a7q5.java

---
+++ OOPs using JAVA/Day 17/a7q5.java
@@ -0,0 +1,16 @@
+// Q5. Write a Java Program to Concatenate Two Strings.
+
+import java.util.*;
+//Author : Abhishek Sharma
+class q5{  
+    public static void main(String args[]){  
+        String s1="Abhishek ";  
+        String s2="Sharma";  
+        String s3=s1.concat(s2);  
+        System.out.println(s3); 
+    }  
+}
+
+// Output :
+// Abhishek Sharma
+
