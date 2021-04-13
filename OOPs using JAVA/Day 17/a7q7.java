3e52bd95ae843fbbab83fcf479fe112deb18cbbe
Abhishek Sharma <sharma.abhishek1282000@gmail.com>
2021-04-13 12:02:13 +0530

    Create a7q7.java

---
+++ OOPs using JAVA/Day 17/a7q7.java
@@ -0,0 +1,13 @@
+//Q7. Write a Java Program to Find the Length of the String.
+import java.util.*;
+//Author : Abhishek Sharma
+public class q7{  
+    public static void main(String args[]){  
+        String s1="Abhishek Sharma";   
+        System.out.println("string length is: "+s1.length());
+    }
+}
+/*
+Output :
+string length is: 15
+*/
