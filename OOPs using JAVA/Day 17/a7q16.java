0d4a392d1f043ff354b98662c50b7a5b6f0cc99a
Abhishek Sharma <sharma.abhishek1282000@gmail.com>
2021-04-13 12:07:43 +0530

    Create a7q16.java

---
+++ OOPs using JAVA/Day 17/a7q16.java
@@ -0,0 +1,16 @@
+// Q16. Write a Java Program to Replace Character or String.
+
+import java.util.*;
+//Author : Abhishek Sharma
+public class q16{  
+    public static void main(String args[]){  
+    String s1="university of engineering and management";  
+    String replaceString=s1.replace('a','e');
+    System.out.println(replaceString);  
+    }
+} 
+
+/*
+Output :
+university of engineering end menegement
+*/
