26c292e841dc957ea332ebf7444517fdca202c88
Abhishek Sharma <sharma.abhishek1282000@gmail.com>
2021-04-18 10:20:38 +0530

    Create a7q38.java

---
+++ OOPs using JAVA/Day 17/a7q38.java
@@ -0,0 +1,23 @@
+// Q38. Write a Java Program to Access the Characters or the ASCII of the Character Available in the String. 
+import java.util.*;
+//Author : Abhishek Sharma
+import java.io.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class q38
+{  
+public static void main(String[] String)   
+{  
+int ch1 = 'a';  
+int ch2 = 'b';  
+System.out.println("The ASCII value of a is: "+ch1);  
+System.out.println("The ASCII value of b is: "+ch2);  
+}  
+}
+
+/*
+Output :
+The ASCII value of a is: 97
+The ASCII value of b is: 98
+*/
