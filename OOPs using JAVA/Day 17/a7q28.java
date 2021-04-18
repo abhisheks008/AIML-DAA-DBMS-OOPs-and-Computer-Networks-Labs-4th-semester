067084f8bf6eadfc2dd341cb3f4182f434ef6fd6
Abhishek Sharma <sharma.abhishek1282000@gmail.com>
2021-04-18 10:04:51 +0530

    Create a7q28.java

---
+++ OOPs using JAVA/Day 17/a7q28.java
@@ -0,0 +1,17 @@
+// Q28. Write a Java Program to Convert a String Totally into Upper Case.
+import java.util.*;
+//Author : Abhishek Sharma
+public class q28 {
+    public static void main(String[] args) {
+        String str = "abhishek sharma";
+        String strUpper = str.toUpperCase();
+        System.out.println("Original String: " + str);
+        System.out.println("String changed to upper case: "+ strUpper);
+    }
+}
+
+/*
+Output :
+Original String: abhishek sharma
+String changed to upper case: ABHISHEK SHARMA
+*/
