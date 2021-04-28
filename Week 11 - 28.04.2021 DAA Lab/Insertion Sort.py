8cd6c7a1d52adeee72d8cb30e6ceb1b4e1aa0e14
Abhishek Sharma <sharma.abhishek1282000@gmail.com>
2021-04-28 16:05:34 +0530

    Create Insertion Sort.py

---
+++ Week 11 - 28.04.2021 DAA Lab/Insertion Sort.py
@@ -0,0 +1,17 @@
+def insertionSort(arr):
+    for i in range(1, len(arr)):
+
+        key = arr[i]
+        j = i-1
+        while j >=0 and key < arr[j] :
+            arr[j+1] = arr[j]
+            j -= 1
+        arr[j+1] = key
+
+
+z = int(input())
+arr = list(map(int, input().split()))
+insertionSort(arr)
+
+for i in range(0,z):
+    print (arr[i], end = " ")
