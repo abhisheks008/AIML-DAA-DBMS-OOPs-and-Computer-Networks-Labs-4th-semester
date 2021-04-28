f4d7857bff24111818f853433331bab75ed99a27
Abhishek Sharma <sharma.abhishek1282000@gmail.com>
2021-04-28 16:05:11 +0530

    Create Fractional Knapsack_28.04.2021.py

---
+++ Week 11 - 28.04.2021 DAA Lab/Fractional Knapsack_28.04.2021.py
@@ -0,0 +1,36 @@
+class ItemValue:
+    def __init__(self, wt, val, ind):
+        self.wt = wt
+        self.val = val
+        self.ind = ind
+        self.cost = val // wt
+    def __lt__(self, other):
+        return self.cost < other.cost
+# Author : Abhishek Sharma
+class FractionalKnapSack:
+    def getMaxValue(wt, val, capacity):
+        iVal = []
+        for i in range(len(wt)):
+            iVal.append(ItemValue(wt[i], val[i], i))
+        iVal.sort(reverse=True)
+        totalValue = 0
+        for i in iVal:
+            curWt = int(i.wt)
+            curVal = int(i.val)
+            if capacity - curWt >= 0:
+                capacity -= curWt
+                totalValue += curVal
+            else:
+                fraction = capacity / curWt
+                totalValue += curVal * fraction
+                capacity = int(capacity - (curWt * fraction))
+                break
+        return totalValue
+# driver code : Author : Abhishek Sharma    
+if __name__ == "__main__":
+    z = int(input())
+    capacity = int(input())
+    wt = list(map(int, input().strip().split()))
+    val = list(map(int, input().strip().split()))
+    maxValue = FractionalKnapSack.getMaxValue(wt, val, capacity)
+    print(int(maxValue))
